296dc19f3c9bc733e82325a32c7d08a18636d3eb
William Safra <61418031+Safra-Man@users.noreply.github.com>
2023-04-27 16:50:44 -0300

    Commit inicial

---
+++ desafioJava/src/test/java/com/avanade/desafioJava/DesafioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.desafioJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
